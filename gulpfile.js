188f1d7399de139595e1498286788b5443483559
unknown <danny@rdbruce.com>
2017-10-02 11:59:07 -0400

    Change theme name | a bunch of other stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
+var themePath = 'src/wp-content/themes/friendshipvillage/lib';
 var sourcemaps = require('gulp-sourcemaps');
-
-// Less configuration
 var gulp = require('gulp');
 var less = require('gulp-less');
+var uglify = require('gulp-uglify');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
 
 gulp.task('less', function() {
-    gulp.src('src/wp-content/themes/fv-firebird/lib/less/*.less')
+    gulp.src([themePath+'/less/style.less', themePath+'/less/homepage.less'])
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('src/wp-content/themes/fv-firebird/lib/css'))
+        .pipe(gulp.dest(themePath+'/css'))
 });
 
-gulp.task('default', ['less'], function() {
-    gulp.watch('src/wp-content/themes/fv-firebird/lib/less/*.less', ['less']);
+gulp.task('compress', function () {
+    gulp.src([themePath+'/js/*.js', '!'+themePath+'/js/*.min.js'])
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(themePath+'/js'))
+});
+
+gulp.task('minify-css', function() {
+    gulp.src([themePath+'/css/style.css', themePath+'/css/homepage.css'])
+        //.pipe(sourcemaps.init())
+        .pipe(cleanCSS())
+        //.pipe(sourcemaps.write())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(themePath+'/css'));
+});
+
+gulp.task('default', ['less', 'minify-css', 'compress'], function() {
+    gulp.watch(themePath+'/less/*.less', ['less', 'minify-css']);
+    gulp.watch([themePath+'/js/*.js', '!'+themePath+'/js/*.min.js'], ['compress']);
+    //gulp.watch([themePath+'/css/*.css', '!'+themePath+'/css/*.min.css'], ['minify-css']);
 })
